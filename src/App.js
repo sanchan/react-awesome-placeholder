9fbdbc4901d20f151dbd653708878cce1a979512
sanchan <danilo.dev@gmail.com>
2017-04-13 21:45:59 +1200

    Adding initial code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React                from 'react'
+import { TablePlaceholder }   from 'components/AwesomePlaceholder'
+
+class App extends React.Component {
+  render() {
+    return (
+      <TablePlaceholder rows={5} />
+    )
+  }
+}
+export default App
